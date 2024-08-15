19f0258951eac29899c7fbf1a70eb397033d998f
IronJayx <iron.jayxxx@gmail.com>
2024-08-15 15:02:33 +0200

    increase max image size

--- airvector/transform/ComputeImageLayout.py
+++ airvector/transform/ComputeImageLayout.py
@@ -1,6 +1,8 @@
 def compute_layout(image_path: str):
-    from PIL import Image
+    import PIL
 
-    image = Image.open(image_path)
+    PIL.Image.MAX_IMAGE_PIXELS = 933120000
+
+    image = PIL.Image.open(image_path)
     width, height = image.size
     return "portrait" if height > width else "landscape"

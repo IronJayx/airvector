2733d6a7d810c81b3610ea8a94dbd391b35f19d0
IronJayx <iron.jayxxx@gmail.com>
2024-09-02 17:37:14 +0200

    remove unused var

--- airvector/transform/OpenAiAzureEmbedding.py
+++ airvector/transform/OpenAiAzureEmbedding.py
@@ -6,8 +6,6 @@ load_dotenv()
 
 
 def get_embedding(text: str) -> list[float]:
-    api_key = os.getenv("AZURE_OPENAI_API_KEY_EMBEDDINGS")
-
     client = AzureOpenAI(
         api_key=os.getenv("AZURE_OPENAI_API_KEY_EMBEDDINGS"),
         api_version="2024-02-01",
